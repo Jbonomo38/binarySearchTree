2178f2b857eb65048e365ab3660968e47170c7ca
Jack <jbonomo38@gmail.com>
2024-10-10 01:19:24 -0400

    Add more functions

--- scripts.js
+++ scripts.js
@@ -36,6 +36,138 @@ class Tree {
 
         return root;
     }
+
+    insert(value, root = this.root) {
+        if(value < root.data) {
+            if(root.left) {
+                this.insert(value, root.left);
+            } else {
+                root.left = new Node(value);
+                return;
+            }
+        } else {
+            if(root.right) {
+                this.insert(value, root.right);
+            } else {
+                root.right = new Node(value);
+                return;
+            }
+        }
+    }
+
+    deleteItem(value, root = this.root) {
+        let deleteVal = this.find(value);
+        let parentVal = this.findParent(value);
+        console.log(deleteVal);
+        console.log(parentVal);
+
+        // Item has no nodes
+        if(this.isLeaf(deleteVal)) {
+            console.log("it's a leaf, baby!");
+            if(parentVal.right === deleteVal) {
+                parentVal.right = null;
+                console.log(parentVal);
+            } else { 
+                parentVal.left = null;
+                console.log(parentVal);
+            }
+            return;
+        };
+
+        // Item has two nodes
+        if(deleteVal.left && deleteVal.right) {
+            let compareVal = deleteVal.right;
+            let parentCompareVal = deleteVal;
+
+            while(compareVal.left !== null) {
+                parentCompareVal = compareVal;
+                compareVal = compareVal.left;
+            }
+            deleteVal.data = compareVal.data;
+
+            if(parentCompareVal.left === compareVal) {
+                parentCompareVal.left = compareVal.right;
+            } else {
+                parentCompareVal.right = compareVal.right;
+            }
+            console.log(compareVal)
+            return;
+        }
+
+        // Item has one node
+        else if(deleteVal.left !== null) {
+            deleteVal.data = deleteVal.left.data;
+            deleteVal.left = null;
+        } else if (deleteVal.right !== null) {
+            deleteVal.data = deleteVal.right.data;
+            deleteVal.right = null;
+        }
+    }
+
+    isLeaf(node) {
+        return (node.left === null && node.right === null)
+    }
+
+    find(value, root = this.root) {
+        if(value === root.data) {
+            return root;
+        } 
+        
+        if(value < root.data) {
+            if(root.left !== null) {
+                return this.find(value, root.left);
+            } else {
+                return new Error("Not in here bruh");
+            }
+        }
+        
+        if(value > root.data) {
+            if(root.right !== null) {
+                return this.find(value, root.right);
+            } else {
+                return new Error("Not in here bruh");
+            }
+        }
+    }
+
+    findParent(value, root = this.root) {
+        if (root.data === value) {
+            return null; // The root has no parent
+        }
+        
+        if(value === root.left.data || value === root.right.data) {
+            return root;
+        } 
+        
+        if(value < root.data) {
+            if(root.left !== null) {
+                return this.findParent(value, root.left);
+            } else {
+                return new Error("Not in here bruh");
+            }
+        }
+        
+        if(value > root.data) {
+            if(root.right !== null) {
+                return this.findParent(value, root.right);
+            } else {
+                return new Error("Not in here bruh");
+            }
+        }
+    }
+
+    levelOrder(callback, node = this.root) {
+        // INCOMPLETE
+        callback(node.data);
+        if(node.left) {
+            this.levelOrder(callback, node.left);
+        }
+
+        if(node.right) {
+            this.levelOrder(callback, node.right);
+        }
+        return;
+    }
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -54,6 +186,18 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 
 const bst = [12, 1, 4, 7, 3, 9, 4, 13, 12, 15, 18];
 const ex = new Tree(bst);
+console.log(ex.sortAndFilter(bst));
 console.log(ex.root);
-console.log(ex.root instanceof Node);
-console.log(prettyPrint(ex.root));
+// ex.insert(16);
+// prettyPrint(ex.root);
+// ex.insert(20);
+// prettyPrint(ex.root);
+// ex.insert(100);
+// prettyPrint(ex.root);
+// console.log(ex.findParent(15));
+// ex.deleteItem();
+// console.log(ex.findParent(12));
+// prettyPrint(ex.root);
+// ex.deleteItem(18);
+prettyPrint(ex.root);
+ex.levelOrder(console.log);
